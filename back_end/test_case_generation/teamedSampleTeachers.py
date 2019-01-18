0013ebba45fc02b99c378cbb734276938321b1a2
Sarah Gold <sarahredhen@yahoo.com>
2019-01-18 12:40:49 -0500

    Created sample 4 with unteamed semesters.

--- back_end/test_case_generation/teamedSampleTeachers.py
+++ back_end/test_case_generation/teamedSampleTeachers.py
@@ -1,4 +1,5 @@
 import random
+import re
 
 teachers = {}
 teachers["James"] = "Jennifer, James, jjames1"
@@ -73,8 +74,10 @@ courseToTeacher["TJ Math 5"] = courseToTeacher["TJ Res Stats 1"]
 courseToTeacher["AP Calculus AB"] = courseToTeacher["TJ Res Stats 1"]
 
 courseToSections = {"TJ Res Stats 1":17, "TJ Math 3":17, "TJ Math 5":2, "TJ Math 4":2, "AP Calculus AB":1, "Biology 1":24, "English 9":24, "Design and Tech":24, "Latin 2":6, "French 2":5, "Spanish 2":9, "Japanese 1":1, "Chinese 1":1, "Russian 1":1, "German 1":1, "Health and PE 9":11, "Foundations CompSci":14, "Foundations CompSci Acc":3, "Symphonic Band":1, "AP Computer Sci A+":1, "Advanced Orchestra":1, "Theatre Arts 1":1, "Art 1":1}
+print(sum(v for v in courseToSections.values()))
 
-courseToNumber = {line.split("| ")[1]:line.split("| ")[2] for line in open("../../runs/constraint_files/newCourses.txt", "r").read().splitlines()}
+courseToNumber = {line.split("| ")[1]:[line.split("| ")[2], line.split("| ")[3]] for line in open("../../runs/constraint_files/newCourses.txt", "r").read().splitlines()}
+numberToCourse = {line.split("| ")[2]:[line.split("| ")[1], line.split("| ")[3]] for line in open("../../runs/constraint_files/newCourses.txt", "r").read().splitlines()}
 
 teacherToSection = {}
 sections = {}
@@ -87,7 +90,10 @@ def assignSections():
     global secID
     print("assignSections")
     # FIND WHERE THIS IS GETTING STUCK IN A LOOP!!!
+    x = 0
     for course in courseToSections.keys():
+        x += 1
+        print("Course: " + course + ", " + x.__str__())
         # Adjustment so that IBET teachers all get six courses. (Adjust to three or six?)
         if course in {"Biology 1", "English 9", "Design and Tech"}:
             t = int(int(courseToSections[course])/6)
@@ -98,21 +104,36 @@ def assignSections():
                 if not teacher in teacherToSection.keys():
                     teacherToSection[teacher] = []
                 for y in range(3):
-                    sections[secID] = [secID.__str__(), "teacher: "+teachers[teacher].split(", ")[2], "courseID: "+courseToNumber[course], "room: ?", "semester: 0","maxstudents: 20", "allowed_periods: 1,2,3"]
+                    sections[secID] = [secID.__str__(), "teacher: "+teachers[teacher].split(", ")[2], "courseID: "+courseToNumber[course][0], "room: ?", "semester: 0","maxstudents: 20", "allowed_periods: 1,2,3"]
                     teacherToSection[teacher].append(secID)
                     secID += 1
                 for y in range(3):
-                    sections[secID] = [secID.__str__(), "teacher: " + teachers[teacher].split(", ")[2], "courseID: " + courseToNumber[course], "room: ?", "semester: 0", "maxstudents: 20", "allowed_periods: 5,6,7"]
+                    sections[secID] = [secID.__str__(), "teacher: " + teachers[teacher].split(", ")[2], "courseID: " + courseToNumber[course][0], "room: ?", "semester: 0", "maxstudents: 20", "allowed_periods: 5,6,7"]
                     teacherToSection[teacher].append(secID)
                     secID += 1
         else:
             for y in range(courseToSections[course]):
                 teacher = random.choice(courseToTeacher[course])
-                while teacher in teacherToSection.keys() and len(teacherToSection[teacher]) >= 5:
-                    teacher = random.choice(courseToTeacher[course])
+                r = 5
                 if not teacher in teacherToSection.keys():
                     teacherToSection[teacher] = []
-                sections[secID] = [secID.__str__(), "teacher: "+teachers[teacher].split(", ")[2], "courseID: "+courseToNumber[course], "room: ?"]
+                for c in teacherToSection[teacher]:
+                    if numberToCourse[re.search(r'[A-Z]*[0-9]+[A-Z]*[0-9]*', sections[c][2]).group(0)][1] == "semester":
+                        r += 1
+                while teacher in teacherToSection.keys() and len(teacherToSection[teacher]) >= r:
+                    teacher = random.choice(courseToTeacher[course])
+                    if not teacher in teacherToSection.keys():
+                        teacherToSection[teacher] = []
+                    r = 5
+                    sem = 0
+                    for c in teacherToSection[teacher]:
+                        if numberToCourse[re.search(r'[A-Z]*[0-9]+[A-Z]*[0-9]*', sections[c][2]).group(0)][1] == "semester":
+                            sem += 1
+                    if sem%2 == 1:
+                        sem += 1
+                    r += sem/2
+                    #print("Hi")
+                sections[secID] = [secID.__str__(), "teacher: "+teachers[teacher].split(", ")[2], "courseID: "+courseToNumber[course][0], "room: ?"]
                 if course in {"TJ Res Stats 1", "TJ Math 4"}:
                     sections[secID].append("semester: 1")
                 if course in {"TJ Math 3", "TJ Math 5"}:
