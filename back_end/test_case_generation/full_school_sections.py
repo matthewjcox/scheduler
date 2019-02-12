dc91b9d0936b7b4fe398bccc15f11399fadf3290
Sarah Gold <sarahredhen@yahoo.com>
2019-02-11 23:01:06 -0500

    Worked on building full school sections list

--- back_end/test_case_generation/full_school_sections.py
+++ back_end/test_case_generation/full_school_sections.py
@@ -27,7 +27,7 @@ secSheet = secList['D1BC6ACA-A78F-4688-9C6D-42D84ED']
 
 # Sections is a dict of sections coded as follows: {secID: [(0) courseID, (1) term, (2) course title, (3) teacher, (4) room, (5) max, (6) [team1], (7) [team2], (8) [team3], (9) [allowedPeriods]}
 sections = {}
-# {teacher: [[(0) secID, (1) period, (2) term, (3) courseID, (4) courseTitle, (5) room, (6) cap]]}
+# {teacher's last name: [[(0) secID, (1) period, (2) term, (3) courseID, (4) courseTitle, (5) room, (6) cap]]}
 # NOTE: term notated as follows: 0 = year, 1 = semester 1, 2 = semester 2
 teacherToSects = {}
 
@@ -35,10 +35,16 @@ teamings = load_workbook(filename='../../runs/constraint_files/Arabic and LS to 
 arabics = teamings['Sheet1']
 learningSems = teamings['Sheet2']
 
+humsAndIbets = load_workbook(filename='../../runs/constraint_files/17-18 HUMs at a glance.xlsx')
+humSheet = humsAndIbets['Sheet3']
+ibetSheet = humsAndIbets['IBETs']
+
+# dict of year-long name of class to the two semester classes it translates to
 teamedToSems = {}
+# dict of semester classes to the year-long classes they're a part of.
 semsToTeam = {}
 
-# Read in data from teaming workbook
+# Read in data from teaming workbook (with Arabics and Learning Seminars)
 for r in range(2, 28):
     curTeam = arabics.cell(row=r, column=1).value
     curSem1 = arabics.cell(row=r, column=10).value
@@ -54,11 +60,33 @@ for r in range(2, 26):
     semsToTeam[curSem1] = curTeam
     semsToTeam[curSem2] = curTeam
 
+# Dict of section IDs to section IDs of clases with which it is teamed (team_1) in HUMs or CHUMs
+secToHum = {}
+# List of sets of teachers who are teamed for IBETs.
+ibets = []
+# Set of IBET course IDs
+ibetIDs = {'4310T1', '984060', '1130T1'}
+#
+
+# Read in data from HUMs and IBETs workbook
+for r in range(1, 19, 3):
+    ibets.append({ibetSheet(row=r, column=1).value, ibetSheet(row=r, column=2).value, ibetSheet(row=r, column=3).value})
+for r in range(1,45):
+    sec1 = humSheet(row=r, column=1).value
+    sec2 = humSheet(row=r, column=5).value
+    sec3 = humSheet(row=r, column=9).value
+    secToHum[sec1] = [sec2]
+    secToHum[sec2] = [sec1]
+    if sec3:
+        secToHum[sec1].append(sec3)
+        secToHum[sec2].append(sec4)
+
+# Read in data from sections workbook
 for r in range(2, 859):
     if secSheet.cell(row=r, column=2).value == 8:
         continue
     if secSheet.cell(row=r, column=6).value:
-        teacher = secSheet.cell(row=r, column=6).value
+        teacher = secSheet.cell(row=r, column=6).value.split(", ")[0]
     elif secSheet.cell(row=r, column=5).value == "See Counselor":
         teacher = "Counselor"
     elif re.search(r'Health & PE', secSheet.cell(row=r, column=5).value):
@@ -84,6 +112,7 @@ for r in range(2, 859):
     teacherToSects[teacher][-1].append(secSheet.cell(row=r, column=7).value)
     teacherToSects[teacher][-1].append(secSheet.cell(row=r, column=9).value)
 
+# Parse data from sections workbook on teacher-by-teacher basis
 for teacher in teacherToSects.keys():
     if teacher == "Gym":
         # Handle gym classes here
@@ -94,10 +123,21 @@ for teacher in teacherToSects.keys():
     perDist = {l:[] for l in range(1,8)}
     for x in range(len(teacherToSects[teacher])):
         perDist[teacherToSects[x][1]].append(x)
-    # Remember to restrict allowed periods for IBETs outside of the restrictions listed here
+    # Remember to restrict allowed periods for IBETs and CHUMs outside of the restrictions listed here
     if perDist.keys() in {1,2,3,4}:
         allwowedPers = {1,2,3,4}
     elif perDist.keys() in {5,6,7}:
         allowedPers = {5,6,7}
     else:
         allowedPers = {1,2,3,4,5,6,7}
+    for per in range(len(perDist.keys())):
+        if len(perDist) == 1:
+            # This puts off handling IBETs. Plan is to access the teachers who teach IBETs after this and team their sections together and restrict periods accordingly. All will happen after main run through teachers.
+            if teacherToSects[teacher][perDist[0][3]] in ibetIDs:
+                continue
+            if teacherToSects[teacher][perDist[0][0]] in
+        perSects = [teacherToSects[teacher][perDist[per][k]] for k in range(len(perDist[per]))]
+
+
+
+
