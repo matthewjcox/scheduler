5efc0becd40f91c7839c24ec64cd545bda0ae103
Sarah Gold <sarahredhen@yahoo.com>
2019-03-13 13:43:30 -0400

    Finished unfulfillable_requests_num.py (In the 2017-2018 case, 281/14849 requests can't be met)

--- back_end/test_case_generation/unfulfillable_requests_num.py
+++ back_end/test_case_generation/unfulfillable_requests_num.py
@@ -2,6 +2,7 @@
 # Spreadsheets needed: 6-1-17@1430.xlsx and SecList 8-14@1300.xlsx
 
 from openpyxl import load_workbook
+import re
 
 stud_wb = load_workbook(filename="../../runs/constraint_files/6-1-17@1430.xlsx")
 sec_wb = load_workbook(filename="../../runs/constraint_files/SecList 8-14@1300.xlsx")
@@ -10,43 +11,73 @@ sec_wb = load_workbook(filename="../../runs/constraint_files/SecList 8-14@1300.x
 sec_ws = sec_wb['D1BC6ACA-A78F-4688-9C6D-42D84ED']
 stud_ws = stud_wb['Sheet1']
 
-# FINISH EDITING THIS!
-raise Exception("Finish collecting number of course requests per courseID")
-while stud_ws.cell(row=r, column=1).value:
-    courseRequests = {}
+# Creates a dictionary of courseID to number of requests for that course
+# Note that variables sectID and sectID2 actually hold courseID data.
+courseRequests = {}
+r = 2
+while stud_ws.cell(row=r, column=4).value:
     if stud_ws.cell(row=r,column=5).value == "See Counselor":
         r+=1
         continue
-    sectID = [stud_ws.cell(row=r, column=4).value.__str__()]
+    sectID = stud_ws.cell(row=r, column=4).value.__str__()
+    sectID2 = None
     if sectID == "503000":
-        courseRequests.append("3190T1")
-        courseRequests.append("313753")
+        sectID = "3190T1"
+        sectID2 = "313753"
     elif sectID == "505000":
-        courseRequests.append("313754")
-        courseRequests.append("316055")
+        sectID = "313754"
+        sectID2 = "316055"
     elif sectID == "501000":
-        courseRequests.append("3190T1")
-        courseRequests.append("314351")
+        sectID = "3190T1"
+        sectID2 = "314351"
     elif sectID == "502000":
-        courseRequests.append("313753")
-        courseRequests.append("313754")
+        sectID = "313753"
+        sectID2 = "313754"
     elif sectID == "504000":
-        courseRequests.append("3190T1")
-        courseRequests.append("313754")
+        sectID = "3190T1"
+        sectID2 = "313754"
     elif sectID == "231904":
-        courseRequests.append("231905")
+        sectID = "231905"
     elif sectID == "2340T1":
-        courseRequests.append("234093")
-    else:
-        courseRequests.append(stud_ws.cell(row=r, column=4).value.__str__())
+        sectID = "234093"
+    if not sectID in set(courseRequests.keys()):
+        courseRequests[sectID] = 0
+    courseRequests[sectID] += 1
+    if sectID2:
+        if not sectID2 in set(courseRequests.keys()):
+            courseRequests[sectID2] = 0
+        courseRequests[sectID2] += 1
     r += 1
-    file.write(curStud.__str__() + ", Student, " + curStud.__str__() + ", " + stud_ws.cell(row=r-1, column=3).value.__str__() + ", " + len(courseRequests).__str__() + "\n")
-    for x in range(len(courseRequests)):
-        file.write(courseRequests[x] + "\n")
-    file.write("\n")
-    #print(curStud)
-    #print(courseRequests)
-    curStud = stud_ws.cell(row=r, column=1).value
 
-# Gets totals for space in 
-for r in range()
+
+# Gets totals for spaces in each course from SecList workbook
+courseMaxes = {}
+r = 2
+while sec_ws.cell(row=r, column=4).value:
+    if re.search('Homeroom', sec_ws.cell(row=r, column=5).value):
+        r+=1
+        continue
+    sectID = sec_ws.cell(row=r, column=4).value.__str__()
+    if sectID == "3190T1" or sectID == "313753" or sectID == "313754" or sectID == "316055" or sectID == "314351" or sectID == "3199T1" or sectID == "3199T2" or sectID == "317860" or sectID == "319800":
+        maxStuds = 28
+    elif sectID == "319966" or sectID == "319967":
+        maxStuds = 25
+    elif sectID == "319916" or sectID == "319917" or sectID == "3199J2" or sectID == "3199J1":
+        maxStuds = 23
+    else:
+        maxStuds = sec_ws.cell(row=r, column=9).value
+    if not sectID in courseMaxes.keys():
+        courseMaxes[sectID] = 0
+    courseMaxes[sectID] += maxStuds
+    r += 1
+
+unfulfillable = 0
+for course in courseRequests.keys():
+    if not course in courseMaxes.keys():
+        courseMaxes[course] = 0
+    if courseRequests[course] - courseMaxes[course] >= 0:
+        unfulfillable += courseRequests[course]-courseMaxes[course]
+
+total = sum([y for y in courseRequests.values()])
+print("Total course requests: " + total.__str__())
+print("Unfillable course requests: " + unfulfillable.__str__())
